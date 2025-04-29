5a4e43ab6969e2b0b6af806e294811660309b8c1
Ayuningtyas24 <adyahseptiani@gmail.com>
2025-04-29 10:46:29 +0700

    menambahkan fungsi

--- introduction.cpp
+++ introduction.cpp
@@ -7,6 +7,12 @@ int nim;
 string nama;
 float nilai;
 
+void print_date(){
+    cout << "NIM = " << nim << endl;
+    cout << "Nama = " << nama << endl;
+    cout << "Nilai = " << nilai << endl;
+}
+
 };
 int main (){
 }
